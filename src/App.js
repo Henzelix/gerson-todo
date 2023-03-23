e7e2fd2da2409d0a73d897436b68975fafee0b09
Mikolaj Henzel <mikolajhenzel@gmail.com>
2023-03-23 02:05:27 +0100

    3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Filter from './Filter'
 import ToDoList from './ToDoList';
 import NewTask from './NewTask';
+import { useState } from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState([["jeden", false], ["dwa", true], ["trzy", false]]);
+
+  console.log(tasks);
+
   return (
     <div className="App">
       <h1>Welcome to my To Do List!</h1>
       <div className='Container'>
         <Filter/>
         <hr></hr>
-        <ToDoList/>
+        <ToDoList tasks={tasks} setTasks={setTasks}/>
         <hr></hr>
         <NewTask/>
       </div>
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,11 +1,18 @@
-function ToDoList(){
+import './App.css'
+
+function ToDoList({ tasks, setTasks }){
+    const listTasks = tasks.map((task) =>
+        <label style={task[1] == true ? { 'textDecoration': 'line-through'} : { 'textDecoration': 'none'}}>
+            <input onClick={() => {console.log('fehefh')}} checked={task[1] == true ? true : false} type={'checkbox'}></input>
+            {task[0]}
+        </label>
+    );
+
     return(
-        <>
-            <ul>
-                <li>jeden</li>
-                <li>dwa</li>
-            </ul>
-        </>
+        <div className="ListOfTasks">
+            {listTasks}
+            <br></br>
+        </div>
     );
 }
 
